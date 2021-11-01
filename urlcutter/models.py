fba6dbe5cac53fe8c984e61d7240aef5d94e20c4
rachel5004 <j26597719@naver.com>
2021-11-01 21:15:42 +0900

    MODIFY:: shortcut column unique

--- urlcutter/models.py
+++ urlcutter/models.py
@@ -3,5 +3,5 @@ from urlcutter import db
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(24), unique=True, nullable=False)
-    shortcut = db.Column(db.String(255))
+    shortcut = db.Column(db.String(255), unique=True)
     create_date = db.Column(db.DateTime(), nullable=False)
